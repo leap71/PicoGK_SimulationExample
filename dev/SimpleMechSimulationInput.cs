a8a52f20ec9d30c3b065b7faf1fb0be82bb231eb
josefinelissner <jl@leap71.com>
2024-06-03 12:24:42 +0400

    Added dev read/write for simple mechanical simulation

---
+++ dev/SimpleMechSimulationInput.cs
@@ -0,0 +1,232 @@
+﻿//
+// SPDX-License-Identifier: CC0-1.0
+//
+// This example code file is released to the public under Creative Commons CC0.
+// See https://creativecommons.org/publicdomain/zero/1.0/legalcode
+//
+// To the extent possible under law, LEAP 71 has waived all copyright and
+// related or neighboring rights to this PicoGK example code file.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+
+using PicoGK;
+
+
+namespace Leap71
+{
+    namespace Simulation
+    {
+        public class SimpleMechSimulationInput
+        {
+            // solid part
+            protected ScalarField   m_oSolidDensityField;       // density in kg/m3
+            protected ScalarField   m_oSolidPoissonField;       // poisson's ration in -
+            protected ScalarField   m_oSolidModulusField;       // young's modulus in Pa
+            protected VectorField   m_oForceField;              // force in N
+            protected VectorField   m_oDisplacementField;       // displacement in m
+            protected Voxels        m_voxSolidDomain;
+
+
+            /// <summary>
+            /// Class to load, analyse the specified VDB file and retrieve input data for a simulation setup.
+            /// The simulation is a simple fluid flow without heat transfer.
+            /// Class retieves a fluid and solid domain and fields for velocity, viscosity and density.
+            /// </summary>
+            public SimpleMechSimulationInput(string strVDBFilePath)
+            {
+                // load 
+                OpenVdbFile vdbfileRead = new(strVDBFilePath);
+                Library.Log($"Loaded VdbFile {strVDBFilePath}");
+
+
+                // check if content is as expected
+                // 1x voxel field is expected
+                // 3x scalar field is expected
+                // 2x vector field is expected
+                uint nNumberOfVoxelFields   = 0;
+                uint nNumberOfVectorFields  = 0;
+                uint nNumberOfScalarFields  = 0;
+                uint nNumberOfFields        = (uint)vdbfileRead.nFieldCount();
+                Library.Log($"VdbFile contains {nNumberOfFields} fields");
+                if (nNumberOfFields != 6)
+                {
+                    throw new Exception(
+                        "Six fields are expected. " +
+                        "VDB file content is not suitable for this simulation input.");
+                }
+
+                for (int nField = 0; nField < nNumberOfFields; nField++)
+                {
+                    string strType = vdbfileRead.strFieldType(nField);
+                    string strName = vdbfileRead.strFieldName(nField);
+                    Library.Log($"-  Field {nField} has type {strType} and name '{strName}'");
+
+                    OpenVdbFile.EFieldType eType = vdbfileRead.eFieldType(nField);
+                    if (eType == OpenVdbFile.EFieldType.VectorField)
+                    {
+                        nNumberOfVectorFields++;
+                    }
+                    else if (eType == OpenVdbFile.EFieldType.Voxels)
+                    {
+                        nNumberOfVoxelFields++;
+                    }
+                    else if (eType == OpenVdbFile.EFieldType.ScalarField)
+                    {
+                        nNumberOfScalarFields++;
+                    }
+                    else
+                    {
+                        throw new Exception(
+                            "Unsupported field found. " +
+                            "VDB file content is not suitable for this simulation input.");
+                    }
+                }
+                if ((nNumberOfVectorFields != 2) ||
+                    (nNumberOfVoxelFields  != 1) ||
+                    (nNumberOfScalarFields != 3))
+                {
+                    throw new Exception(
+                        "Two vector fields are expected. " +
+                        "One voxel field is expected. " +
+                        "Three scalar fields are expected. " +
+                        "VDB file content is not suitable for this simulation input.");
+                }
+
+
+                // retrieve fields
+                for (int nField = 0; nField < nNumberOfFields; nField++)
+                {
+                    string strName = vdbfileRead.strFieldName(nField);
+                    OpenVdbFile.EFieldType eType = vdbfileRead.eFieldType(nField);
+
+                    // solid domain voxel field
+                    if ((eType == OpenVdbFile.EFieldType.Voxels) &&
+                        (strName.Contains(SimulationKeyWords.m_strSolidKey) == true))
+                    {
+                        m_voxSolidDomain = vdbfileRead.voxGet(nField);
+                        Library.Log($"-  Field '{strName}' successfully retrieved as solid domain voxel field.");
+                    }
+
+                    // displacement vector field
+                    else if ((eType == OpenVdbFile.EFieldType.VectorField) &&
+                             (strName.Contains(SimulationKeyWords.m_strDisplacementKey) == true))
+                    {
+                        m_oDisplacementField = vdbfileRead.oGetVectorField(nField);
+                        Library.Log($"-  Field '{strName}' successfully retrieved as displacement vector field.");
+                    }
+
+                    // force vector field
+                    else if ((eType == OpenVdbFile.EFieldType.VectorField) &&
+                             (strName.Contains(SimulationKeyWords.m_strForceKey) == true))
+                    {
+                        m_oForceField = vdbfileRead.oGetVectorField(nField);
+                        Library.Log($"-  Field '{strName}' successfully retrieved as force vector field.");
+                    }
+
+                    // density scalar field
+                    else if ((eType == OpenVdbFile.EFieldType.ScalarField) &&
+                             (strName.Contains(SimulationKeyWords.m_strDensityKey) == true))
+                    {
+                        m_oSolidDensityField = vdbfileRead.oGetScalarField(nField);
+                        Library.Log($"-  Field '{strName}' successfully retrieved as density scalar field.");
+                    }
+
+                    // young's modulus scalar field
+                    else if ((eType == OpenVdbFile.EFieldType.ScalarField) &&
+                             (strName.Contains(SimulationKeyWords.m_strModulusKey) == true))
+                    {
+                        m_oSolidModulusField = vdbfileRead.oGetScalarField(nField);
+                        Library.Log($"-  Field '{strName}' successfully retrieved as young's modulus scalar field.");
+                    }
+
+                    // poisson's ratio scalar field
+                    else if ((eType == OpenVdbFile.EFieldType.ScalarField) &&
+                             (strName.Contains(SimulationKeyWords.m_strPoissonKey) == true))
+                    {
+                        m_oSolidPoissonField = vdbfileRead.oGetScalarField(nField);
+                        Library.Log($"-  Field '{strName}' successfully retrieved as poisson's ratio scalar field.");
+                    }
+                }
+
+                if (m_oForceField == null)
+                    throw new MissingFieldException("Missing force field");
+
+                if (m_oDisplacementField == null)
+                    throw new MissingFieldException("Missing displacement field");
+
+                if (m_oSolidDensityField == null)
+                    throw new MissingFieldException("Missing solid density field");
+
+                if (m_oSolidModulusField == null)
+                    throw new MissingFieldException("Missing solid young's modulus field");
+
+                if (m_oSolidPoissonField == null)
+                    throw new MissingFieldException("Missing solid poisson's ratio field");
+
+                if (m_voxSolidDomain == null)
+                    throw new MissingFieldException("Missing solid domain voxel field");
+            }
+
+            /// <summary>
+            /// Returns the solid domain as a voxel field.
+            /// </summary>
+            public Voxels voxGetSolidDomain()
+            {
+                return m_voxSolidDomain;
+            }
+
+            /// <summary>
+            /// Returns the displacements as a vector field.
+            /// All values are specified in m.
+            /// </summary>
+            public VectorField oGetDisplacementField()
+            {
+                return m_oDisplacementField;
+            }
+
+            /// <summary>
+            /// Returns the forces as a vector field.
+            /// All values are specified in N.
+            /// </summary>
+            public VectorField oGetForceField()
+            {
+                return m_oForceField;
+            }
+
+            /// <summary>
+            /// Returns the solid's densities as a scalar field.
+            /// All values are specified in kg/m3.
+            /// </summary>
+            public ScalarField oGetDensityField()
+            {
+                return m_oSolidDensityField;
+            }
+
+            /// <summary>
+            /// Returns the young's moduli as a scalar field.
+            /// All values are specified in Pa.
+            /// </summary>
+            public ScalarField oGetYoungModulusField()
+            {
+                return m_oSolidModulusField;
+            }
+
+            /// <summary>
+            /// Returns the poisson ratios as a scalar field.
+            /// All values are specified in -.
+            /// </summary>
+            public ScalarField oGetPoissonRatioField()
+            {
+                return m_oSolidPoissonField;
+            }
+        }
+    }
+}
---
+++ dev/SimpleMechSimulationOutput.cs
@@ -0,0 +1,164 @@
+﻿//
+// SPDX-License-Identifier: CC0-1.0
+//
+// This example code file is released to the public under Creative Commons CC0.
+// See https://creativecommons.org/publicdomain/zero/1.0/legalcode
+//
+// To the extent possible under law, LEAP 71 has waived all copyright and
+// related or neighboring rights to this PicoGK example code file.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+
+using PicoGK;
+using System.Numerics;
+
+
+namespace Leap71
+{
+    using ShapeKernel;
+
+    namespace Simulation
+    {
+        public class SimpleMechSimulationOutput
+        {
+            // solid part
+            protected ScalarField   m_oSolidDensityField;       // density in kg/m3
+            protected ScalarField   m_oSolidPoissonField;       // poisson's ration in -
+            protected ScalarField   m_oSolidModulusField;       // young's modulus in Pa
+            protected VectorField   m_oForceField;              // force in N
+            protected VectorField   m_oDisplacementField;       // displacement in m
+            protected Voxels        m_voxSolidDomain;
+
+
+            /// <summary>
+            /// Class to create a VDB file from physical and geometric input data.
+            /// This information can be used for automated simulation setup.
+            /// </summary>
+            public SimpleMechSimulationOutput(  string strVDBFilePath,
+                                                float  fSolidDensity,
+                                                float  fSolidPoissonRatio,
+                                                float  fSolidYoungModulus,
+                                                Voxels voxSolidDomain,
+                                                Voxels voxFixedDisplacementPatch,
+                                                Voxels voxAppliedForcePatch)
+            {
+                // set domains
+                m_voxSolidDomain            = voxSolidDomain;
+
+
+                //todo: set voxFixedDisplacementPatch to not null
+                //todo: generate displacement patch
+                //todo: fix displacement to zero in all voxels inside the patch?
+                voxFixedDisplacementPatch = Sh.voxIntersect(m_voxSolidDomain, voxFixedDisplacementPatch);
+                Vector3 vecFixed            = new Vector3(0, 0, 0);
+                VectorField oFixedField     = new VectorField(voxFixedDisplacementPatch, vecFixed);
+
+
+                //todo: merge with displacement vector field for entire solid domain
+                //todo: Vectores vecFixed and vecDefault are the same. More then one vector field needed?
+                Vector3 vecDefault          = new Vector3(0, 0, 0);
+                m_oDisplacementField        = new VectorField(m_voxSolidDomain, vecDefault);
+                VectorFieldMerge.Merge(oFixedField, m_oDisplacementField);
+
+
+                //todo: set voxAppliedForcePatch to not null
+                //todo: generate force patch
+                //todo: assume force as 20 N in x-dir
+                //todo: apply force vector to all voxels inside the patch?
+                voxAppliedForcePatch        = Sh.voxIntersect(m_voxSolidDomain, voxAppliedForcePatch);
+                Vector3 vecForce            = new Vector3(20, 0, 0); 
+                VectorField oForcedField    = new VectorField(voxAppliedForcePatch, vecForce);
+
+
+                //todo: merge with force vector field for entire solid domain
+                m_oForceField               = new VectorField(m_voxSolidDomain, vecDefault);
+                VectorFieldMerge.Merge(oForcedField, m_oForceField);
+
+
+                // density scalar field from voxel field
+                m_oSolidDensityField = ScalarFieldUtil.oGetConstScalarField(m_oDisplacementField, fSolidDensity);
+
+
+                // modulus scalar field from voxel field
+                m_oSolidModulusField = ScalarFieldUtil.oGetConstScalarField(m_oDisplacementField, fSolidYoungModulus);
+
+
+                // poisson scalar field from voxel field
+                m_oSolidPoissonField = ScalarFieldUtil.oGetConstScalarField(m_oDisplacementField, fSolidPoissonRatio);
+
+
+                // write VDB file
+                OpenVdbFile oFile = new();
+                oFile.nAdd(m_voxSolidDomain,        $"Simulation.Domain_{SimulationKeyWords.m_strSolidKey}");
+                oFile.nAdd(m_oDisplacementField,    $"Simulation.Field_{SimulationKeyWords.m_strDisplacementKey}");
+                oFile.nAdd(m_oForceField,           $"Simulation.Field_{SimulationKeyWords.m_strForceKey}");
+                oFile.nAdd(m_oSolidDensityField,    $"Simulation.Field_{SimulationKeyWords.m_strDensityKey}");
+                oFile.nAdd(m_oSolidModulusField,    $"Simulation.Field_{SimulationKeyWords.m_strModulusKey}");
+                oFile.nAdd(m_oSolidPoissonField,    $"Simulation.Field_{SimulationKeyWords.m_strPoissonKey}");
+                oFile.SaveToFile(strVDBFilePath);
+                Library.Log($"Exported VdbFile {strVDBFilePath} successfully.");
+            }
+
+            /// <summary>
+            /// Returns the solid domain as a voxel field.
+            /// </summary>
+            public Voxels voxGetSolidDomain()
+            {
+                return m_voxSolidDomain;
+            }
+
+            /// <summary>
+            /// Returns the displacements as a vector field.
+            /// All values are specified in m.
+            /// </summary>
+            public VectorField oGetDisplacementField()
+            {
+                return m_oDisplacementField;
+            }
+
+            /// <summary>
+            /// Returns the forces as a vector field.
+            /// All values are specified in N.
+            /// </summary>
+            public VectorField oGetForceField()
+            {
+                return m_oForceField;
+            }
+
+            /// <summary>
+            /// Returns the solid's densities as a scalar field.
+            /// All values are specified in kg/m3.
+            /// </summary>
+            public ScalarField oGetDensityField()
+            {
+                return m_oSolidDensityField;
+            }
+
+            /// <summary>
+            /// Returns the young's moduli as a scalar field.
+            /// All values are specified in Pa.
+            /// </summary>
+            public ScalarField oGetYoungModulusField()
+            {
+                return m_oSolidModulusField;
+            }
+
+            /// <summary>
+            /// Returns the poisson ratios as a scalar field.
+            /// All values are specified in -.
+            /// </summary>
+            public ScalarField oGetPoissonRatioField()
+            {
+                return m_oSolidPoissonField;
+            }
+        }
+    }
+}
